4f7bdf365e14f4229291d77acb43be8ab63ed807
erkobridee <erko.bridee@gmail.com>
2023-01-24 19:16:50 +0100

    feat(project): add production optimization

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,13 @@
+const isProduction = `${process.env.NODE_ENV}` === 'production';
+
 module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     autoprefixer: {},
+
+    // https://tailwindcss.com/docs/optimizing-for-production
+    ...(isProduction ? { cssnano: {} } : {}),
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 
+import { name } from './package.json';
+
+const nodeEnv = `${process.env.NODE_ENV}`;
+
+console.log(`package name: ${name}`);
+console.log(`environment: ${nodeEnv}`);
+
+const isProduction = `${nodeEnv}` === 'production';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  ...(isProduction ? { base: `/${name}/` } : {}),
   plugins: [react(), tsconfigPaths()],
 });
