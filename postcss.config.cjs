49a6a1fa5a7102e902c87339b9b48fbee6fcf182
erkobridee <erko.bridee@gmail.com>
2025-07-22 09:17:59 +0200

    update(postcss): config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,6 @@ const isProduction = `${process.env.NODE_ENV}` === 'production';
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
     '@tailwindcss/postcss': {},
     autoprefixer: {},
 
