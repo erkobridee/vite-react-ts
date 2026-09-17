b025fdd56eb6deb1778e92974fdb8488a506b927
erkobridee <erko.bridee@gmail.com>
2026-09-17 14:57:44 -0300

    update(eslint): config

--- eslint.config.js
+++ eslint.config.js
@@ -10,13 +10,9 @@ export default defineConfig([
   globalIgnores(['dist', '**/*.d.ts']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
     languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+    plugins: { reactHooks, reactRefresh },
   },
   eslintConfigPrettier,
 ]);
