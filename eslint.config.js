6045cedb17f25cfaf65fa12e41ff9da568d3b974
erkobridee <erko.bridee@gmail.com>
2026-09-18 11:22:09 -0300

    update(eslint): to version 10

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,40 @@
-import js from '@eslint/js';
 import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
 
-export default defineConfig([
-  globalIgnores(['dist', '**/*.d.ts']),
+import react from '@ternaus/eslint-plugin-react';
+import tsParser from '@typescript-eslint/parser';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { reactRefresh } from 'eslint-plugin-react-refresh';
+
+export default [
+  // Global ignores - applies to ALL configs below
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [js.configs.recommended, tseslint.configs.recommended],
-    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
-    plugins: { reactHooks, reactRefresh },
+    ignores: ['node_modules/**', 'dist/**'],
   },
+
+  {
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh.plugin,
+    },
+    rules: {
+      ...react.configs.flat.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+
   eslintConfigPrettier,
-]);
+];
